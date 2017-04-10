fb1ed70677fee1d5d431223d64c3317fd6a8f3c3
Angie Vanegas <arco527@gmail.com>
2017-04-10 10:04:05 -0500

    Add favicon, gup file named public

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
+  var gulp = require('gulp');
+  var sass = require('gulp-sass');
+  var rename = require('gulp-rename');
 
 
-gulp.task('styles', function(){
-  gulp
-    .src('index.scss')
-    .pipe(sass())
-    .pipe(rename('app.css'))
-    .pipe(gulp.dest('public'));
-})
+  gulp.task('styles', function(){
+    gulp
+      .src('index.scss')
+      .pipe(sass())
+      .pipe(rename('app.css'))
+      .pipe(gulp.dest('public'));
+  })
 
-gulp.task('default', ['styles'])
+  gulp.task('assets', function(){
+    gulp
+      .src('assets/*')
+      .pipe(gulp.dest('public'));
+
+  })
+
+  gulp.task('default', ['styles', 'assets'])
