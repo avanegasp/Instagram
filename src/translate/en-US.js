eebbeb4532a406e79877f9175228effc6eb62d55
Angie Vanegas <arco527@gmail.com>
2017-05-11 19:05:55 -0500

    Add languaje spanish and english texts

--- src/translate/en-US.js
+++ src/translate/en-US.js
@@ -2,5 +2,20 @@
     likes:'{ likes, plural, ' +
             '=0 {no likes}' +
             '=1 {# like}' +
-            'other { # likes }}'
+            'other { # likes }}',
+            'logout': 'Logout',
+            'english': 'English',
+            'spanish': 'Spanish',
+            'signup.subheading': 'Signup to watch your pictures pet\'s and friend\'s',
+            'signup.facebook': 'Signup with Facebook',
+            'signup.text': 'Signup',
+            'email': 'Email',
+            'username': 'Username',
+            'fullname': 'Fullname',
+            'password': 'Password',
+            'signup.call-to-action': 'Signup',
+            'signup.have-account': '¿Already have an account?',
+            'signin': 'Signin',
+            'signin.not-have-account': '¿Don\'t have an account?',
+            'language': 'Language'
   }
--- src/translate/es.js
+++ src/translate/es.js
@@ -1,3 +1,18 @@
   module.exports = {
-    likes:'{ likes, number } me gusta'
+    'likes':'{ likes, number } me gusta',
+    'logout': 'Salir',
+    'english': 'Inglés',
+    'spanish': 'Español',
+    'signup.subheading': 'Regístrate para ver fotos de tus mascotas y amigos',
+    'signup.facebook': 'Iniciar sesión con facebook',
+    'signup.text': 'Iniciar sesión',
+    'email': 'Correo Electrónico',
+    'username': 'Nombre de usuario',
+    'fullname': 'Nombre completo',
+    'password': 'Contraseña',
+    'signup.call-to-action': 'Regístrate',
+    'signup.have-account': '¿Tienes una cuenta?',
+    'signin': 'Entrar',
+    'signin.not-have-account': '¿No tienes una cuenta?',
+    'language': 'Idioma'
   }
